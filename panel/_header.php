b8a35346f96d50daa44d02768a744c2d8eb78ed0
Israrsahib303 <165788330+Israrsahib303@users.noreply.github.com>
2025-11-30 11:29:32 +0500

    Update: panel/_header.php

--- panel/_header.php
+++ panel/_header.php
@@ -213,7 +213,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
             <a class="nav-link <?= ($current_page == 'menus.php') ? 'active' : '' ?>" href="menus.php">
                 <i class="fa-solid fa-list"></i> User menu nav 
             </a>
-            <a class="nav-link <?= ($current_page == 'githubphp') ? 'active' : '' ?>" href="menus.php">
+            <a class="nav-link <?= ($current_page == 'github_sync.php') ? 'active' : '' ?>" href="github_sync.php">
                 <i class="fa-brands fa-github"></i> GitHub Sync
             </a>
             <a class="nav-link text-danger mt-3" href="../logout.php">
